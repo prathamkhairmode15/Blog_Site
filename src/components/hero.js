ea76122d2ebc9fe63e7659d6b4167329f4e09cbb
Pratham Sachin Khairmode <prathamkhairmode15@gmail.com>
2026-02-22 12:21:46 +0530

    source

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,7 +2,7 @@ export function initHero() {
     const hero = document.getElementById('hero-section');
     hero.innerHTML = `
         <div class="hero-wrapper">
-            <img src="public/assets/images/hero.jpg" 
+            <img src="/assets/images/hero.jpg" 
                  alt="Featured Post">
             <div class="hero-text-content">
                 <h1>Documenting My Journey: One Day at a Time</h1>
